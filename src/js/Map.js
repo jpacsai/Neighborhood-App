2ec85efe282e543baff79d84ebc42f6d3b1397b5
jpacsai <judit.pacsai@gmail.com>
2018-07-19 00:44:07 +0200

    feat: google map

---
+++ src/js/Map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 46.883448,
+      lng: 17.437112
+    },
+    zoom: 12
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyA5ivLlpxg-AwsOTPELxcuO1zQ64Vo6yRo' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default Map;
