7c12785d4508292dbb5eefbdb0434518654646bb
jpacsai <judit.pacsai@gmail.com>
2018-07-18 23:36:58 +0200

    style: tidy up code

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import Map from './components/map';
 import Filters from './components/filters';
 import PlaceListing from './components/place-listing';
+import Map from './components/map';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
+      
         <header>
           <h1>Title</h1>
           <Filters />
         </header>
+
         <main>
           <section>
             <h2>Places</h2>
@@ -19,6 +22,7 @@ class App extends Component {
           </section>
           <Map />
         </main>
+
       </div>
     );
   }
