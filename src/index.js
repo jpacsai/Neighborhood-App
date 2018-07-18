ee3ec4ec0fbe7d8b1b50ddf913fc849cfe8cb5ee
jpacsai <judit.pacsai@gmail.com>
2018-07-18 23:04:20 +0200

    add redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './js/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
+import allReducers from './js/reducers/comb-reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(allReducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
