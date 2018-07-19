8ef6052d2a8cc5868333f8e96d0ed15b64078fbb
jpacsai <judit.pacsai@gmail.com>
2018-07-19 15:54:27 +0200

    add list placeholder

--- src/js/components/Filters.js
+++ src/js/components/Filters.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 const Filters = () => (
             <section>
                 <h2>Filters</h2>
+                <select>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
             </section>
 )
 
