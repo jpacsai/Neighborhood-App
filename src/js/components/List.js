9bc99fed97803b67c7798e3f004bc54c84c8e601
jpacsai <judit.pacsai@gmail.com>
2018-07-22 20:55:26 +0200

    style: tidy up code

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -4,9 +4,8 @@ const List = (props) => {
     if (props.list) {
         return props.list.map((event) => {
             const date = event.dates.start.localDate || 'unknown date';
-            const time = event.dates.start.localTime ? (event.dates.start.localTime).slice(0, 5) : '';
+            const time = event.dates.start.localTime ? (event.dates.start.localTime).slice(0, 5) : null;
             const location = event._embedded.venues[0].city.name || 'unkown location';
-            console.log('event ',event);
             return (
                 <li key={ event.id } className='event'>{ event.name }
                     { <p>Date: { date } { time } Location: { location }</p> }
