3dd2597db9378943bb9855e80c6c0249a267c629
jpacsai <judit.pacsai@gmail.com>
2018-07-22 20:36:48 +0200

    fix: set default values for event properties

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -3,10 +3,13 @@ import React from 'react';
 const List = (props) => {
     if (props.list) {
         return props.list.map((event) => {
+            const date = event.dates.start.localDate || 'unknown date';
+            const time = event.dates.start.localTime || 'unkown time';
+            const location = event._embedded.venues[0].city.name || 'unkown location';
             console.log('event ',event);
             return (
                 <li key={ event.id } className='event'>{ event.name }
-                    { <p>Date: { event.dates.start.localDate } { (event.dates.start.localTime).slice(0,5) } Location: { event._embedded.venues[0].city.name }</p> }
+                    { <p>Date: { date } { time } Location: { location }</p> }
                 </li>
             )
         })
