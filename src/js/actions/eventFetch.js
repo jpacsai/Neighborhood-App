d401a7c9f0edacc76151ea10a89a4b2527b01021
jpacsai <judit.pacsai@gmail.com>
2018-07-25 00:11:49 +0200

    fix: fetch url day date format

--- src/js/actions/eventFetch.js
+++ src/js/actions/eventFetch.js
@@ -5,14 +5,18 @@ export function loadData(){
     let date = new Date();
     let month = (date.getMonth()+1) + '';
     month = month.length < 2 ? '0' + month : month;
+    let day = (date.getDate() + '');
+    day =  day.length < 2 ? '0' + day : day;
     
-    const today = date.getFullYear() + '-' + month + '-' + date.getDate();
+    const today = date.getFullYear() + '-' + month + '-' + day;
 
     date.setDate(date.getDate() + 7);
     month = (date.getMonth()+1) + '';
     month = month.length < 2 ? '0' + month : month;
+    day = (date.getDate() + '');
+    day =  day.length < 2 ? '0' + day : day;
 
-    const nextWeek = date.getFullYear() + '-' + month + '-' + date.getDate();
+    const nextWeek = date.getFullYear() + '-' + month + '-' + day;
 
     const url = 'https://app.ticketmaster.com/discovery/v2/events.JSON?classificationName=music&apikey=GEKqrkYNGL9POROireeB9D6fzdpWP8dj&page=0&size=50&marketId=204&startDateTime=' + today + 'T00:00:00Z&endDateTime=' + nextWeek + 'T23:59:00Z&includeTBA=no&includeTBD=no';
 
