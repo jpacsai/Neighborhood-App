e2017156715757cf3539649ae8b6f8930d919c3e
jpacsai <judit.pacsai@gmail.com>
2018-07-25 16:15:35 +0200

    refactor: move filters into modal component

--- src/js/components/FilterModal.js
+++ src/js/components/FilterModal.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Filters from '../components/Filters';
 import { filterEvents } from './../actions/filterEvents';
 import { hideDatePicker } from './../actions/hideDatePicker';
-import FilterModal from './../components/FilterModal';
+import DatePicker from './../components/DayPicker';
 
-class FilterContainer extends Component {
+class FilterModal extends Component {
 
     render() {
         const { events, locations } = this.props;
 
         return (
-            <section className={ 'filter-container' }>
-                <h2 className={ ['aside-header', 'aside-header-filter'].join(' ') }>Filters</h2>
-                <button type="button" onClick={ () => this.props.hideDatePicker(this.props.isHidden) }>
-                    { (this.props.isHidden  && 'Show Filters') || 'Hide Filters'}
-                </button>
-                
-                < FilterModal />
-            </section>
+            <form onSubmit={ (e) => 
+                this.props.filterEvents(e, this.props.events, this.props.locationFilter) }>
+                    <DatePicker />
+                    <Filters title={ 'Location' } list={ locations } events={ events }/>
+                    <button onClick={ () => this.props.hideDatePicker(this.props.isHidden) }>Filter</button>
+            </form> 
         )
     }
 }
@@ -38,4 +37,4 @@ function mapDispatchToProps(dispatch) {
     }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FilterContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(FilterModal);
