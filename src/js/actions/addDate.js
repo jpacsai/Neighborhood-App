c5dd5e797678db6a57dca1aaf61e7ac56c8bc3aa
jpacsai <judit.pacsai@gmail.com>
2018-07-26 22:33:27 +0200

    fix: add date obj to events

--- src/js/actions/addDate.js
+++ src/js/actions/addDate.js
@@ -1,9 +1,17 @@
 export function addDateProperty(response) {
     console.log('add date', response._embedded.events);
-    // const withDateObj = 
+    const events = response._embedded.events;
+    const addDateObj = events.map(event => { 
+        const date = event.dates.start.localDate;
+        const obj = new Date(date.slice(0,4), parseInt(date.slice(5, 7)-1, 10), parseInt(date.slice(8,10), 10));
+        event.dates.start.dateObj = obj;
+        return event;
+    });
+
     return{
         type:"EVENT_FETCH_SUCCESS",
         events: {
-            events: response._embedded.events},
+            events: addDateObj
+        }
     }
 }
