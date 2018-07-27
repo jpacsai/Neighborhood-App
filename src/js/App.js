4b73ccc67de9482f888fc502f994822ea4317bd8
jpacsai <judit.pacsai@gmail.com>
2018-07-27 12:17:47 +0200

    feat: sort event list by name

--- src/js/App.js
+++ src/js/App.js
@@ -31,6 +31,18 @@ class App extends Component {
 		}
 	}
 
+	sortByAlphabet(arr) {
+		if (arr) {
+			const newArr = arr.slice(0);
+			newArr.sort((a, b) => {
+				var textA = a.name;
+				var textB = b.name;
+				return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+			})
+			return newArr;
+		}
+	}
+
 	render() {
 		const displayList = this.props.filteredEvents.length === 0 ? this.sortByDate(this.props.events) : 
 			this.props.filteredEvents === 'no match found' ? '' : this.sortByDate(this.props.filteredEvents);
