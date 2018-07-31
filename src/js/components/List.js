3df7542b33645b4b07ac83d3430af1228e3bdfa4
jpacsai <judit.pacsai@gmail.com>
2018-07-31 14:18:35 +0200

    refactor: transform stateless List component to stateful

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -1,44 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { closeUp } from './../actions/closeUp';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-const List = (props) => {
+class List extends Component {
 
-    const { list, closeUp, showInfo } = props;
+    render() {
 
-    if (list === 'no match') {
-        return (
-            <li className='event'>No match found</li>
-        )
-    }
-
-    else if (list) {
-        return list.map( (event) => {
-
-            const date = event.dates.start.localDate;
-            const time = event.dates.start.localTime ? (event.dates.start.localTime).slice(0, 5) : null;
-            const location = event._embedded.venues[0].city.name;
-            
+        const { list, closeUp, showInfo } = this.props;
+    
+        if (list === 'no match') {
             return (
-                <li 
-                    key={ event.id } 
-                    className='event'
-                    onClick={ () => {
-                        console.log(event.name);
-                        closeUp(showInfo);
-                    }}
-                >
-                    { event.name }
-                    <p>Date: { date } { time } Location: { location }</p>
-                </li>
+                <li className='event'>No match found</li>
             )
-        })
+        }
+    
+        else if (list) {
+            return list.map( (event) => {
+    
+                const date = event.dates.start.localDate;
+                const time = event.dates.start.localTime ? (event.dates.start.localTime).slice(0, 5) : null;
+                const location = event._embedded.venues[0].city.name;
+                
+                return (
+                    <li 
+                        key={ event.id } 
+                        className='event'
+                        onClick={ () => {
+                            console.log(event.name);
+                            closeUp(showInfo);
+                        }}
+                    >
+                        { event.name }
+                        <p>Date: { date } { time } Location: { location }</p>
+                    </li>
+                )
+            })
+        }
+    
+        else {
+            return null;
+        }
     }
 
-    else {
-        return null;
-    }
 }
 
 function mapStateToProps(state) {
